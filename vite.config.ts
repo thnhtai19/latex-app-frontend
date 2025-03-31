61ff73270f885a6f38eb3a4f5c386252f8090adf
Trần Thành Tài <tai.tt@geekup.vn>
2025-03-31 20:57:09 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-  },
-  preview: {
-    allowedHosts: ["latex.ttdev.id.vn", "localhost"],
-  },
+	server: {
+		cors: {
+			origin: ['https://latex.ttdev.id.vn', 'http://localhost:4173'],
+			methods: ['GET', 'POST'],
+			allowedHeaders: ['Content-Type']
+		},
+		allowedHosts: ['latex.ttdev.id.vn']
+	}
 });
