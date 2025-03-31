bc9af4fac0726b12463219bd39ee8d353c3b560f
Trần Thành Tài <tai.tt@geekup.vn>
2025-03-31 13:01:15 +0700

    feat: fix docker

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 443,
+    },
+    allowedHosts: ["latex.ttdev.id.vn"],
+  },
 });
