d571ad24207471ee6ee3463341dfc0964f16c877
Trần Thành Tài <tai.tt@geekup.vn>
2025-03-31 20:43:58 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 4173,
-    strictPort: true,
-    hmr: {
-      clientPort: 443,
-    },
-    allowedHosts: ["all"],
   },
   preview: {
-    host: "0.0.0.0",
-    port: 4173,
-    allowedHosts: ["all"],
+    allowedHosts: ["latex.ttdev.id.vn", "localhost"],
   },
 });
